d3d816d321356282f58d5aa9031b88353781eee7
Halil ÖZEL <halil.ozel@veripark.com>
2021-12-30 16:59:16 +0300

    SuspendCoroutines lesson was added.

---
+++ app/src/main/java/com/halil/ozel/coroutines101/SuspendCoroutines.kt
@@ -0,0 +1,26 @@
+package com.halil.ozel.coroutines101
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+
+    runBlocking {
+        delay(2000)
+        println("Run Blocking")
+        myFunction()
+    }
+
+}
+
+/**
+It allows the completion of operations without blocking the current running thread.
+Suspend; These are functions that can be restarted and stopped. These functions should be used in the coroutine.
+ */
+suspend fun myFunction() {
+    coroutineScope {
+        delay(4000)
+        println("Suspend Function")
+    }
+}
